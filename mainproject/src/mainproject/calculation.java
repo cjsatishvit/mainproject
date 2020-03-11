50ad8bc5ba4b8d00a41f9bc26b157baf1b049559
cjsatishvit <cjsatishvit2@gmail.com>
2020-03-12 05:28:09 +0530

    adding the project baseline code

---
+++ mainproject/src/mainproject/calculation.java
@@ -0,0 +1,14 @@
+package mainproject;
+
+public class calculation {
+	
+	public void add(int x,int y)
+	{
+		System.out.println(x+y);
+	}
+
+	public void subtract(int x,int y)
+	{
+		System.out.println(x-y);
+	}
+}
